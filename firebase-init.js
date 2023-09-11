aab8aab8665166562cc334ae00a9078388b244f0
Victor Spinella Moreira <78853634+victorspinella@users.noreply.github.com>
2023-09-11 15:15:54 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC2PJ_iq9y7vJ7J0HxTrtLjPtvWyYbDa-s",
+    authDomain: "teste-8921a.firebaseapp.com",
+    projectId: "teste-8921a",
+    storageBucket: "teste-8921a.appspot.com",
+    messagingSenderId: "963960311684",
+    appId: "1:963960311684:web:04fd70dd878061b78517d1",
+    measurementId: "G-RJJEFZ1SMT"
+};
+firebase.initializeApp(firebaseConfig);
---
+++ pages/home/home.js
@@ -0,0 +1,10 @@
+function logout() {
+    firebase.auth().signOut().then(() => {
+        window.location.href = "../../index.html";
+    }).catch(() => {
+        alert('Erro ao fazer logout');
+    })
+}
+
+
+
